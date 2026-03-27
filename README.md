f71a652a7f3f963ea0b8c0f346449b13e7122a81
Creciunel <creciunelcatalin@gmail.com>
2026-03-27 14:39:52 +0000

    Refactor code structure for improved readability and maintainability

--- README.md
+++ README.md
@@ -4,6 +4,42 @@ Welcome to the Zephyr RTOS training! This repository includes a ready-to-use
 development environment based on Zephyr 4.3.0, which you can set up in one of
 three ways:
 
+## Virtual Environment
+
+```bash
+source ~/zephyrproject/.venv/bin/activate
+```
+
+```bash
+west init -l
+
+west update
+```
+
+```bash
+
+export ZEPHYR_BASE=~/zephyrproject/zephyr
+
+west build --board doit_esp32_devkit_v1/esp32/procpu app -p
+
+west flash
+```
+
+---
+## For Virtual Machine
+
+Alow esp32 device
+
+```bash
+sudo fuser -k /dev/ttyUSB0
+sudo chmod 666 /dev/ttyUSB0
+```
+
+---
+## Results
+
+![Results](first_flash.png)
+
 ---
 
 ## Manual Zephyr Setup
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -5,9 +5,9 @@
 #define SLEEP_TIME_MS 1000
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
+// #define LED_NODE DT_ALIAS(led0)
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+// static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
@@ -15,12 +15,12 @@ int main(void)
 {
     bool led_state = true;
 
-    if (!gpio_is_ready_dt(&led)) return 0;
+    // if (!gpio_is_ready_dt(&led)) return 0;
 
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    // if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
 
     while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+        // if (gpio_pin_toggle_dt(&led) < 0) return 0;
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
